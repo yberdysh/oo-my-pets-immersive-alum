f0126797b14ae7d9300d682757af280bddfaa749
Jane Costa <yberdysh@gmail.com>
2018-06-28 14:13:47 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
   # code goes here
+  attr_reader :name, :mood
+  attr_writer :mood
+  def initialize(name)
+  	@name = name
+  	@mood = "nervous"
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
   # code goes here
+  attr_reader :species
+  @@all = []
+  def self.all
+  	@@all
+  end
+
+  def self.count
+  	self.all.length
+  end
+
+  def self.reset_all
+  	self.all.clear
+  end
+
+
+  def initialize(species)
+  	@species = species
+  	@@all << species
+  end
+
 end
